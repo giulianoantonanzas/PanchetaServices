a8161e72c5264bc04ac08c4d2d783d0b7d3847e6
showadock <lautaro.scholtus@gmail.com>
2022-02-06 05:06:52 -0300

    Añado user

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-// Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("pancheta_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
-  //logging: console.log,
 });
